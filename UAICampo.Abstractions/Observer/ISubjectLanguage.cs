95d5d990302ecd0ffcf6f537342fd3d820d35552
Tomás Bodas <tomasbodas@hotmail.com>
2022-05-30 22:06:28 -0300

    Observer

---
+++ UAICampo.Abstractions/Observer/ISubjectLanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAICampo.Abstractions.Observer
+{
+    public interface ISubjectLanguage
+    {
+        void Add(IObserverUser user);
+        void Remove(IObserverUser user);
+        void Notification();
+    }
+}
---
+++ UAICampo.Services/Observer/Language.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAICampo.Abstractions.Observer;
+
+namespace UAICampo.Services.Observer
+{
+   
+     public class Language : ILanguage , ISubjectLanguage
+    {
+        public string Name { get; set; }
+
+        private List<IObserverUser> users;
+        public void Add(IObserverUser user)
+        {
+            if (users.Contains(user))
+            {
+                users.Add(user);
+            }
+            else
+            {
+                Interaction.MsgBox("");
+            }
+        }
+
+        public void Notification()
+        {
+            foreach (var user in users)
+            {
+                user.Update(this);
+            }
+
+            if (users.Count == 0)
+            {
+                Interaction.MsgBox("");
+            }
+        }
+
+        public void Remove(IObserverUser user)
+        {
+            if (users.Contains(user))
+            {
+                users.Remove(user);
+            }
+            else
+            {
+                Interaction.MsgBox("");
+            }
+        }
+    }
+}
